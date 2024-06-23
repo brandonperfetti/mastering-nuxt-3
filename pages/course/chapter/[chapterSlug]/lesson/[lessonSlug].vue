1f88fbfa218c055330cdd27409562e442689173a
Brandon Perfetti <brandon@brandonperfetti.com>
2024-06-23 13:07:43 -0700

    1.7 Add Video Player Component.

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,30 +1,31 @@
 <template>
 	<div>
 		<p class="mt-0 uppercase font-bold text-slate-400 mb-1">
-			Lesson {{ chapter.number }} - {{ lesson.number }}
+			Lesson {{ chapter?.number }} - {{ lesson?.number }}
 		</p>
-		<h2 class="my-0">{{ lesson.title }}</h2>
+		<h2 class="my-0">{{ lesson?.title }}</h2>
 		<div class="flex space-x-4 mt-2 mb-8">
 			<a
-				v-if="lesson.sourceUrl"
+				v-if="lesson?.sourceUrl"
 				class="font-normal text-md text-gray-500"
 				:href="lesson.sourceUrl"
 			>
 				Download Source Code
 			</a>
 			<a
-				v-if="lesson.downloadUrl"
+				v-if="lesson?.downloadUrl"
 				class="font-normal text-md text-gray-500"
 				:href="lesson.downloadUrl"
 			>
 				Download Video
 			</a>
 		</div>
-		<p>{{ lesson.text }}</p>
+		<VideoPlayer v-if="lesson?.videoId" :videoId="lesson.videoId" />
+		<p>{{ lesson?.text }}</p>
 	</div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const course = useCourse()
 const route = useRoute()
 
@@ -35,7 +36,7 @@ const chapter = computed(() => {
 })
 
 const lesson = computed(() => {
-	return chapter.value.lessons.find(
+	return chapter.value?.lessons.find(
 		lesson => lesson.slug === route.params.lessonSlug,
 	)
 })
