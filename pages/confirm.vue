5e7090eda9c52925ebf2d122da924ea6b920f90e
Brandon Perfetti <brandon@brandonperfetti.com>
2024-06-25 16:00:38 -0700

    4.8 Protecting Routes with Auth.

---
+++ pages/confirm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>Waiting for login...</div>
+</template>
+
+<script setup lang="ts">
+const user = useSupabaseUser();
+const { query } = useRoute();
+
+watch(
+  user,
+  () => {
+    if (user.value) {
+      const to = (query.redirectTo as string) ?? '/';
+      return navigateTo(to, {
+        replace: true,
+      });
+    }
+  },
+  { immediate: true }
+);
+</script>
