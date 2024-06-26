c04c8525f336d88823e2ca6c30cd3a388563acd2
Brandon Perfetti <brandon@brandonperfetti.com>
2024-06-26 16:43:57 -0700

    Fix type errors.

--- server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
+++ server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
@@ -10,8 +10,19 @@ course as Course;
 
 export default defineEventHandler(
   (event): LessonWithPath => {
-    const { chapterSlug, lessonSlug } =
-      event.context.params;
+    const params = event.context.params as {
+      chapterSlug: string;
+      lessonSlug: string;
+  } | undefined;
+
+  if (!params) {
+      throw createError({
+          statusCode: 404,
+          message: 'Parameters are missing',
+      });
+  }
+
+  const { chapterSlug, lessonSlug } = params;
 
     const chapter: Maybe<Chapter> = course.chapters.find(
       (chapter) => chapter.slug === chapterSlug
