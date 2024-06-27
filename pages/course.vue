405b4d8604d0be94f434fe4dde7711c96d68ef72
Brandon Perfetti <brandon@brandonperfetti.com>
2024-06-26 17:28:40 -0700

    5.8 Fetch Course Metadata.

--- pages/course.vue
+++ pages/course.vue
@@ -5,7 +5,7 @@
     >
       <h1 class="text-3xl">
         <span class="font-medium">
-          <span class="font-bold">{{ title }}</span>
+          <span class="font-bold">{{ course.title }}</span>
         </span>
       </h1>
       <UserCard />
@@ -18,7 +18,7 @@
         <h3>Chapters</h3>
         <div
           class="space-y-1 mb-4 flex flex-col"
-          v-for="chapter in chapters"
+          v-for="chapter in course.chapters"
           :key="chapter.slug"
         >
           <h4>{{ chapter.title }}</h4>
@@ -65,13 +65,12 @@
   </div>
 </template>
 
-<script setup lang="ts">
-const { chapters, title } = useCourse();
+<script setup>
+const course = await useCourse();
+const firstLesson = await useFirstLesson();
 
 const resetError = async (error) => {
-  await navigateTo(
-    '/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3'
-  );
+  await navigateTo(firstLesson.path);
   error.value = null;
 };
 </script>
