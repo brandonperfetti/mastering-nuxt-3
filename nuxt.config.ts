1a16edd34bf2055984eef3ac67656c565380d297
Brandon Perfetti <brandon@brandonperfetti.com>
2024-06-28 14:18:22 -0700

    7.4 Managing Static Assets.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,19 @@
+import vsharp from 'vite-plugin-vsharp';
+
 export default defineNuxtConfig({
   nitro: {
     prerender: {
       routes: ['/landing'],
     },
   },
+  vite: {
+    plugins: [vsharp()],
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@vueuse/nuxt',
     '@nuxtjs/supabase',
-    '@pinia/nuxt'
+    '@pinia/nuxt',
   ],
   supabase: {
     redirectOptions: {
