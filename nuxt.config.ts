73df4bce844281dc1f24b1a3ee674a93494dc01b
Brandon Perfetti <brandon@brandonperfetti.com>
2024-06-29 10:32:49 -0700

    7.7 Setting up Stripe.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 import vsharp from 'vite-plugin-vsharp';
 
 export default defineNuxtConfig({
+  runtimeConfig: {
+    stripeSecret: '',
+    public: {
+      stripeKey: '',
+    },
+  },
   nitro: {
     prerender: {
       routes: ['/landing'],
