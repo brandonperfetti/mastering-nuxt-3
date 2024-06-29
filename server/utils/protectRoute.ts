7dab2c613b51bb6f5f740e7e41d00444175041c3
Brandon Perfetti <brandon@brandonperfetti.com>
2024-06-29 12:09:50 -0700

    7.15 Grant Access to the Course.

--- server/utils/protectRoute.ts
+++ server/utils/protectRoute.ts
@@ -1,8 +1,30 @@
 import { H3Event } from 'h3';
 
 // If the user does not exist on the request, throw a 401 error
-export default (event: H3Event) => {
+export default async (event: H3Event) => {
   if (!event.context.user) {
+    throw createError({
+      statusCode: 401,
+      message: 'Unauthorized',
+    });
+  }
+
+  // Check to see if this user has access to this course
+  // Retrieve cookie, ensuring it's either a string or undefined
+  const cookieHeader = getHeader(event, 'cookie');
+
+  // Prepare headers, conditionally including the cookie if it exists
+  const headers: HeadersInit = {};
+  if (cookieHeader) {
+    headers['cookie'] = cookieHeader;
+  }
+
+  // Check to see if this user has access to this course
+  const hasAccess = await $fetch('/api/user/hasAccess', {
+    headers: headers,
+  });
+
+  if (!hasAccess) {
     throw createError({
       statusCode: 401,
       message: 'Unauthorized',
